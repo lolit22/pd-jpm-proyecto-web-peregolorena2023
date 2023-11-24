c47eb8223c40d04571e4937d40b5147221d9288f
lolit22 <lorena.e.perego@gmail.com>
2023-11-24 04:47:14 -0300

    Agrego libreria confetti JavaScript

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const jsConfetti = new JSConfetti ();
+
+    jsConfetti.addConfetti({
+        emojis:['🔥','🎉','❤️','🧀','🤎','🍔','🫦','💛','💣','💥'], 
+    })
+})
